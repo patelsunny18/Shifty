12732608b944e1a99d4145c7f3fa1e8167af2ac5
sunny <psunny1411@gmail.com>
2022-03-15 19:28:24 -0600

    Separated the JS code from ejs files

---
+++ public/js/addManager.js
@@ -0,0 +1,301 @@
+const form = document.querySelector("#form");
+const fName = document.querySelector("#fName");
+const lName = document.querySelector("#lName");
+const address = document.querySelector("#address");
+const phone = document.querySelector("#phone");
+const dob = document.querySelector("#dob");
+const email = document.querySelector("#email");
+const bankAccount = document.querySelector("#bankAccount");
+const sinNumber = document.querySelector("#sinNumber");
+const avail = document.querySelector("#avail");
+const monday = document.querySelector("#monday");
+const tuesday = document.querySelector("#tuesday");
+const wednesday = document.querySelector("#wednesday");
+const thursday = document.querySelector("#thursday");
+const friday = document.querySelector("#friday");
+const saturday = document.querySelector("#saturday");
+const sunday = document.querySelector("#sunday");
+const mon_choice = document.querySelector("#mon-options");
+const tue_choice = document.querySelector("#tue-options");
+const wed_choice = document.querySelector("#wed-options");
+const thurs_choice = document.querySelector("#thurs-options");
+const fri_choice = document.querySelector("#fri-options");
+const sat_choice = document.querySelector("#sat-options");
+const sun_choice = document.querySelector("#sun-options");
+
+
+monday.addEventListener('change', function () {
+    if (monday.checked) {
+        mon_choice.disabled = false;
+    } else if (!monday.checked) {
+        mon_choice.disabled = true;
+    }
+})
+
+tuesday.addEventListener('change', function () {
+    if (tuesday.checked) {
+        tue_choice.disabled = false;
+    } else if (!tuesday.checked) {
+        tue_choice.disabled = true;
+    }
+})
+
+wednesday.addEventListener('change', function () {
+    if (wednesday.checked) {
+        wed_choice.disabled = false;
+    } else if (!wednesday.checked) {
+        wed_choice.disabled = true;
+    }
+})
+
+thursday.addEventListener('change', function () {
+    if (thursday.checked) {
+        thurs_choice.disabled = false;
+    } else if (!thursday.checked) {
+        thurs_choice.disabled = true;
+    }
+})
+
+friday.addEventListener('change', function () {
+    if (friday.checked) {
+        fri_choice.disabled = false;
+    } else if (!friday.checked) {
+        fri_choice.disabled = true;
+    }
+})
+
+saturday.addEventListener('change', function () {
+    if (saturday.checked) {
+        sat_choice.disabled = false;
+    } else if (!saturday.checked) {
+        sat_choice.disabled = true;
+    }
+})
+
+sunday.addEventListener('change', function () {
+    if (sunday.checked) {
+        sun_choice.disabled = false;
+    } else if (!sunday.checked) {
+        sun_choice.disabled = true;
+    }
+})
+
+fName.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(fName, "First name can't be blank");
+    } else {
+        setSuccessFor(fName);
+    }
+})
+
+lName.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(lName, "Last name can't be blank");
+    } else {
+        setSuccessFor(lName);
+    }
+})
+
+address.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(address, "Address can't be blank");
+    } else {
+        setSuccessFor(address);
+    }
+})
+
+phone.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(phone, "Phone can't be blank");
+    } else if (!this.value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+        setErrorFor(phone, "Incorrect phone number");
+    } else {
+        setSuccessFor(phone);
+    }
+})
+
+
+dob.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(dob, "Date of birth can't be blank");
+    } else {
+        setSuccessFor(dob);
+    }
+})
+
+email.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(email, "Email can't be blank");
+    } else {
+        setSuccessFor(email);
+    }
+})
+
+bankAccount.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(bankAccount, "Bank account can't be blank");
+    } else {
+        setSuccessFor(bankAccount);
+    }
+})
+
+sinNumber.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(sinNumber, "SIN number can't be blank");
+    } else {
+        setSuccessFor(sinNumber);
+    }
+})
+
+
+function checkInputs() {
+    const fNameValue = fName.value.trim();
+    const lNameValue = lName.value.trim();
+    const addressValue = address.value.trim();
+    const phoneValue = phone.value.trim();
+    const dobValue = dob.value.trim();
+    const emailValue = email.value.trim();
+    const bankAccountValue = bankAccount.value.trim();
+    const sinNumberValue = sinNumber.value.trim();
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    let success = false;
+
+    if (fNameValue === '') {
+        setErrorFor(fName, "First name can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(fName);
+        success = true;
+    }
+
+    if (lNameValue === '') {
+        setErrorFor(lName, "Last name can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(lName);
+        success = true;
+    }
+
+    if (addressValue === '') {
+        setErrorFor(address, "Address can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(address);
+        success = true;
+    }
+
+    if (phoneValue === '') {
+        setErrorFor(phone, "Phone can't be blank");
+    } else if (!phoneValue.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+        setErrorFor(phone, "Incorrect phone number");
+    } else {
+        setSuccessFor(phone);
+    }
+
+    if (dobValue === '') {
+        setErrorFor(dob, "Date of birth can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(dob);
+        success = true;
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, "Email can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(email);
+        success = true;
+    }
+
+    if (bankAccountValue === '') {
+        setErrorFor(bankAccount, "Bank account number can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(bankAccount);
+        success = true;
+    }
+
+    if (sinNumberValue === '') {
+        setErrorFor(sinNumber, "SIN number can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(sinNumber);
+        success = true;
+    }
+
+    if (checkboxes.length === 0) {
+        const control = avail.parentElement;
+        const small = control.querySelector("small");
+        small.innerText = "Availability can't be blank";
+        control.className = 'col control error';
+        success = false;
+    } else if (checkboxes.length !== 0) {
+        const control = avail.parentElement;
+        control.className = 'col control success';
+        success = true;
+    }
+    return success;
+}
+
+function setErrorFor(input, message) {
+    const control = input.parentElement.parentElement;
+    const small = control.querySelector("small");
+    small.innerText = message;
+    control.className = 'col control error';
+}
+
+function setSuccessFor(input) {
+    const control = input.parentElement.parentElement;
+    control.className = 'col control success';
+}
+
+form.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+    if (!checkInputs()) {
+        event.preventDefault();
+    } else {
+        sendData(event);
+    }
+}
+
+function sendData(event) {
+    event.preventDefault();
+
+    const fNameValue = fName.value.trim();
+    const lNameValue = lName.value.trim();
+    const addressValue = address.value.trim();
+    const phoneValue = phone.value.trim();
+    const dobValue = dob.value.trim();
+    const emailValue = email.value.trim();
+    const bankAccountValue = bankAccount.value.trim();
+    const sinNumberValue = sinNumber.value.trim();
+    const availability = {}
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    const periods = document.querySelectorAll('select:enabled');
+    for (let i = 0; i < periods.length; i++) {
+        availability[checkboxes[i].id] = periods[i].value;
+    }
+
+    const data = {
+        firstName: fNameValue,
+        lastName: lNameValue,
+        address: addressValue,
+        phoneNumber: phoneValue,
+        dob: dobValue,
+        email: emailValue,
+        bankAccount: bankAccountValue,
+        sinNumber: sinNumberValue,
+        availability: availability,
+    }
+
+    axios.post('/addManager', data)
+        .then((result) => {
+            alert(`New manager added successfully!\n\nUserID: ${result.data.userID}\nPassword: ${result.data.password}`);
+        })
+        .catch((error) => {
+            alert("Oops! Something went wrong. Please, try again.");
+        })
+}
---
+++ public/js/login.js
@@ -0,0 +1,108 @@
+const form = document.querySelector("#form");
+const username = document.querySelector("#userID");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit", handleFormSubmit);
+
+username.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(username, "Username can't be blank");
+    } else {
+        setSuccessFor(username)
+    }
+});
+
+password.addEventListener("input", function (event) {
+    if (this.value === '') {
+        setErrorFor(password, "Password can't be blank");
+    } else {
+        setSuccessFor(password)
+    }
+})
+
+function handleFormSubmit(event) {
+    if (!checkInputs()) {
+        event.preventDefault();
+    } else {
+        sendData(event);
+    }
+}
+
+function sendData(e) {
+    e.preventDefault();
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    const data = {
+        userID: usernameValue,
+        password: passwordValue
+    }
+
+    axios.post('/', data)
+        .then((response) => {
+            switch (response.data.role) {
+                case "Owner": {
+                    console.log(response.data.id);
+                    window.location = `/owner/${response.data.id}`;
+                    break;
+                }
+                case "Manager": {
+                    window.location = `/manager/${response.data.id}`;
+                    break;
+                }
+                case "Employee": {
+                    window.location = `/employee/${response.data.id}`;
+                    break;
+                }
+            }
+        })
+        .catch((error) => {
+            form.reset();
+            checkInputs();
+            alert("User not found. Please try again!")
+        })
+}
+
+function checkInputs() {
+    // get the values from the inputs
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    let success = false;
+
+    if (usernameValue === '') {
+        setErrorFor(username, "Username can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(username)
+        success = true;
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, "Password can't be blank");
+        success = false;
+    } else {
+        setSuccessFor(password);
+        success = true;
+    }
+
+    return success;
+}
+
+function setErrorFor(input, message) {
+    const control = input.parentElement;
+    const small = control.querySelector("small");
+
+    small.innerText = message;
+    control.className = 'control error';
+}
+
+function setSuccessFor(input) {
+    const control = input.parentElement;
+    control.className = 'control success';
+}
+
+        // function checkPassword(input) {
+        //     const pass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,14}$/;
+
+        //     return input.match(pass);
+        // }
